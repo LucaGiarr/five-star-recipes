6d3248ad4d8e5196aaa78536239185e656c0aa7c
LucaGiarr <>
2023-05-21 16:31:18 +0000

    add starters page (html, view and url)

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path('', views.RecipeList.as_view(), name='home'),
+    path('starters/', views.StartersList.as_view(), name='starters'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -7,3 +7,9 @@ class RecipeList(generic.ListView):
     model = Recipe
     queryset = Recipe.objects.filter(status=1).order_by('-created_on')
     template_name = 'index.html'
+
+
+class StartersList(generic.ListView):
+    model = Recipe
+    queryset = Recipe.objects.filter(status=1, category=0)
+    template_name = 'starters.html'
