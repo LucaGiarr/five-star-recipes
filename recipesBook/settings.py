6245efbc8148a28b168330799d64d174ac69c2d5
LucaGiarr <>
2023-06-10 14:17:31 +0000

    deployment commit

--- recipesBook/settings.py
+++ recipesBook/settings.py
@@ -29,7 +29,9 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
+
+X_FRAME_OPTION = 'SAMEORIGIN'
 
 ALLOWED_HOSTS = ['8000-lucagiarr-five-star-reci-h8z6uc98ax.us2.codeanyapp.com',
                  'five-star-recipes.herokuapp.com']
@@ -105,13 +107,6 @@ WSGI_APPLICATION = 'recipesBook.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
 
 DATABASES = {
     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
